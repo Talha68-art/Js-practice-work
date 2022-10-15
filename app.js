42adfdc59693bdb7e519dfffea76bfedb1d23145
talha <talha@gmail.com>
2022-10-15 22:54:20 +0500

    Js class 01

---
+++ app.js
@@ -0,0 +1,324 @@
+// alert("welcome");
+
+// var firstName = "talha";
+// firstName = "TALHA RAFIQ";
+// console.log(typeof(firstName));
+
+// var nationality;
+// nationality = "Pakistani";
+// console.log(nationality);
+
+// var firstVal = 2;
+// var secVal = 5;
+// var total = firstVal + secVal;
+// console.log(total);
+
+// var firstVal = 2;
+// var secVal = 5;
+// var total = firstVal - secVal;
+// console.log(total);
+
+// var firstName = "talha";
+// var lastName = "rafiq";
+// console.log(firstName+lastName);
+
+// var firstVal = Number(prompt("Enter the number"));
+// var secVal = 3;
+// var total = firstVal % secVal;
+// console.log(total);
+
+
+// qUESTION # 01
+
+// var firstVal = Number(prompt("Enter the Value"));
+// var secVal = 3;
+// var total = firstVal % secVal;
+// console.log(total);
+
+// if( total == 0){
+//     console.log("Divisible by 3");
+
+// }
+// else{
+//     console.log("Not divisible by 3");
+// }
+
+// Question number 02
+
+// var firstVal = Number(prompt("Enter the Value"));
+// var secVal = 2;
+// var total = firstVal % secVal;
+// console.log(total);
+
+// if( total == 0){
+//     console.log("Even number");
+
+// }
+// else{
+//     console.log("Odd number");
+// }
+
+
+/// Question # 03
+
+// var age = prompt("Enter your Age") 
+
+// if( age >= 18 ){
+// console.log("Old enough");
+// }
+// else{
+//     console.log("Too Young");
+// }
+
+// Question number 04
+
+// var userName= prompt("Enter your name");
+
+// if(userName=="Talha"){
+// alert("welcome to our page")
+// }
+// else{
+//     alert("sorry ")
+
+// }
+
+
+/// Question number 13
+
+// var firstNum = Number(prompt("Please enter the number"));
+// var secNum = Number(prompt("Please enter the number"));
+
+// if (firstNum > secNum) {
+//     console.log(firstNum, " is greater than", secNum);
+// }
+// else if (firstNum == secNum) {
+//     console.log(firstNum, "is equal to ", secNum);
+// }
+// else {
+//     console.log(firstNum, "is less than", secNum);
+// }
+
+
+//  Question number 09
+
+// var firstVal = Number(prompt("Enter the Year YYYY"));
+// var secVal = 4;
+// var total = firstVal % secVal;
+// console.log(total);
+
+// if( total == 0){
+//     console.log("Leap Year");
+
+// }
+// else{
+//     console.log("Not a leap year");
+// }
+
+// Question number 06 ---------============
+// var firstVal = prompt("enter the value");
+
+// var secVal = 1;
+// var total = firstVal % secVal;
+
+// if(total == 0 ){
+//     console.log("value is Number");
+// }
+// else{
+// console.log("Value is string");
+// }
+
+
+// Question number 17--------===========
+
+// var string = prompt("Enter the value");
+
+// if(string== "a" ||  string == "e" || string== "i" || string== "o" || string== "u"){
+// console.log(true);
+// }
+// else if(string== "A" ||  string == "E" || string== "I" || string== "O" || string== "U"){
+//     console.log(true);
+// }
+// else{
+// console.log(false);
+// }
+
+// Question number 18
+// var userInput = Number(prompt("Enter the number"));
+
+// if( 10 != 8){
+// console.log(true);
+// }
+// else{
+// console.log(false);
+// }
+
+
+// Question number 07-------------===========
+
+
+// var firstInput = Number(prompt("Enter the first value"));
+// var operator = prompt("Operator");
+// var secondInput = Number(prompt("Enter the second value"));
+// console.log(operator);
+
+// if (operator == "+") {
+//     console.log(firstInput + secondInput);
+// }
+// else if (operator == "-") {
+//     console.log(firstInput - secondInput);
+// }
+// else if (operator == "*") {
+//     console.log(firstInput * secondInput);
+// }
+// else if (operator == "/") {
+//     console.log(firstInput / secondInput);
+// }
+
+// else {
+//     console.log("sorrry");
+// }
+
+
+// var userInput = Number(prompt('Enter any number'));
+// var newInput = 1;
+// var total = userInput + newInput;
+// console.log(total);
+//  console.log(userInput);
+// console.log(typeof userInput);
+
+// if( total ="NaN" ){
+// console.log("Input is String");
+// }
+// else if (total= 0){
+// console.log("Input is number")
+// }
+// else{
+// console.log("NaN");
+// }
+
+
+// Question number 15-------------===========>>>>
+
+// var userInput = Number(prompt("What is the current Hour"));
+
+// if (userInput >= 6 && userInput <= 9) {
+//     console.log("BreakFast is served");
+// }
+// else if (userInput >= 11 && userInput <= 13) {
+//     console.log("Time for Lunch");
+// }
+// else if (userInput >= 17 && userInput <= 20) {
+//     console.log("It's dinner time");
+// }
+// else {
+//     console.log("Sorry ,you will have to wait");
+// }
+
+
+// Question number 08-------------===========>>>>
+
+
+// var userInput = Number(prompt("What is the current Hour"));
+
+// if (userInput >= 00 && userInput <= 11.59) {
+//     console.log("Good Morning");
+// }
+// else if (userInput >= 12 && userInput <= 16.59) {
+//     console.log("Good evening");
+// }
+// else if (userInput >= 17 && userInput <= 20.59) {
+//     console.log("Good evening");
+// }
+// else if (userInput >= 21 && userInput <= 23.59) {
+//     console.log("Good night");
+// }
+// else {
+//     console.log("Please Enter the Correct Time ");
+// }
+
+
+// Question number 10-------------===========>>>>
+
+
+// var userInput = prompt("Please Enter the Correct Password");
+// var password = "Pakistan123456";
+
+// if(userInput == password){
+// alert("Correct! The password you entered matches the original password");
+// }
+// else{
+// alert("Incorrect Password");
+// }
+
+
+// Question number 11-------------===========>>>>
+
+
+// var userInput = prompt("Please Enter the Name");
+
+// if(userInput == "Fahad"){
+// document.write("Hello Fahad");
+// }
+// else{
+// document.write("You are not Fahad");
+// }
+
+// Question number 14-------------===========>>>>
+
+// var userInput = Number(prompt("Enter the number"));
+
+// if(userInput > 0){
+// console.log("Positive")
+// }
+// else if(userInput < 0){
+//     console.log("Negative")
+//     }
+//  else if(userInput == 0){
+//         console.log("Zero")
+//         }
+// else{
+// console.log("sorry");
+// }
+
+
+var firstVal = prompt("enter the value");
+
+var secVal = 1;
+var total = firstVal % secVal;
+console.log(total);
+
+if(total == 0 ){
+    console.log("value is Number");
+}
+else if(firstVal === "true" || firstVal ==="false"){
+    console.log("Value is boolean");
+}
+else{
+console.log("Value is string");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
